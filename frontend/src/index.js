70bdf64cb7205ea3a3585bf1df0874175cf02cc5
_woo_s.j <sjwoo1999@korea.ac.kr>
2025-03-05 19:48:55 +0900

    🔧 Modified: frontend/src/index.js
    Modified: frontend/src/index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,7 +17,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
