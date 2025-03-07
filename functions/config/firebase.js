227f7dd42f656cbdb8d7acbdcd2fc60bdc83c24c
_woo_s.j <sjwoo1999@korea.ac.kr>
2025-03-07 10:07:04 +0900

    🔧 Modified: deploy.yml, firebase.js
    Modified: deploy.yml, firebase.js

--- functions/config/firebase.js
+++ functions/config/firebase.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // serviceAccountKey.json을 직접 로드해 인증 (개발 환경)
 // 만약 프로덕션에서 Application Default Credentials를 쓰고 싶다면, 조건 분기도 가능
-const serviceAccount = require('../serviceAccountKey.json');
+const serviceAccount = require('./serviceAccountKey.json');
 
 if (!admin.apps.length) {
   admin.initializeApp({
